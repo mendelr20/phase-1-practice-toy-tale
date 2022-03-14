ea1dd4f9773909f3734c4b2c0df73bf5d0dbe8ba
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-03-14 08:37:47 -0400

    commit tree ba415f0b2bd139ddc6356955389349f53d841fc8

--- src/index.js
+++ src/index.js
@@ -1,89 +1,67 @@
-function allToyData(){
+function toyData(){
   fetch('http://localhost:3000/toys')
-    .then(res => res.json())
-    .then((data) => {
-      data.forEach(displayToys)
-    })
-  }
+  .then(res => res.json())
+  .then(data => data.forEach(toyDisplay))
+}
 
-let addToy = false;
-
-
-function displayToys(toy){
-   let name = document.createElement('p')
-   name.textContent = toy.name
-   let img = document.createElement('img')
-   img.src = toy.image
-   console.log(toy)
-   img.className = "toy-avatar"
-   
-   let likes = toy.likes
-   let likeButton = document.createElement('button')
-   likeButton.id = "like"
-   likeButton.textContent = `${likes} likes`
-   likeButton.addEventListener('click', () => {
-     likes ++
-     likeButton.textContent = `${likes} likes`
-   })
-   
-   let div = document.createElement('div')
-   div.className = "card"
-   div.append(name)
-   div.appendChild(img)
-   div.appendChild(likeButton)
-   document.querySelector("#toy-collection").appendChild(div)
-  }
+function toyDisplay(toy){
+  let name = document.createElement('p')
+  name.textContent = toy.name
+  let img = document.createElement('img')
+  img.src = toy.image
+  img.className = "toy-avatar"
   
-allToyData()
+  let likes = toy.likes
+  let likeButton = document.createElement('button')
+  likeButton.id = "like"
+  likeButton.textContent = `${likes} likes`
+  likeButton.addEventListener('click', () =>{
+  likes ++
+  likeButton.textContent = `${likes} likes`
+  })
+  let div = document.createElement('div')
+  div.className = "card"
+  div.append(name)
+  div.appendChild(img)
+  div.appendChild(likeButton)
+  document.querySelector("#toy-collection").appendChild(div)
+  }
 
+  let submitBtn = document.querySelector("#addToyBtn")
+  submitBtn.addEventListener('click', submitForm)
+  let formName = document.querySelector("input[name='name']")
+  let imgUrl = document.querySelector("input[name='image']")
 
-let submitBtn = document.querySelector("#addToyBtn")
-
-
-submitBtn.addEventListener('click', submit)
-let formName = document.querySelector("input[name='name']")
-let imgUrl = document.querySelector("input[name='image']")
- 
-function submit(e){
+function submitForm(e){
   e.preventDefault()
   let submitInfo = {
-  name: formName.value,
-  image: imgUrl.value
+    name: formName.value,
+    image: imgUrl.value
   }
   submitPost(submitInfo)
   formName.value=''
   imgUrl.value=''
 }
 
-
-
-
 function submitPost(submitInfo){
-  console.log(submitInfo)
   fetch('http://localhost:3000/toys', {
-  method: "POST",
-  headers:
-  {
-    "Content-Type": "application/json",
-    Accept: "application/json"
-  },
-  
-  body: JSON.stringify({
-    "name": submitInfo.name,
-    "image": submitInfo.img,
-    "likes": 0
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "name": submitInfo.name,
+      "image": submitInfo.img,
+      "likes": 0
+    })
   })
-})
-.then((res) => {
-  return res.json()
-})
-.then((data) => {
-  console.log(data)
-  displayToys(data)
-})
+  .then((res) => {
+    return res.json()
+  })
+  .then(data => toyData(data))
 }
-
-
+toyData()
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -99,4 +77,5 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   
 });
+let addToy = false;
 
